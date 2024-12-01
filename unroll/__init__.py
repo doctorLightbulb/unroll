d874b15d2fab5ef9fb29fccc57cbdb66cb1382c8
Doctor I. Q. Lightbulb <64180446+doctorLightbulb@users.noreply.github.com>
2024-11-30 17:12:21 -0800

    Removed ez_setup.py in favor of pyproject.toml and changed midi2strikes to midi2keystrikes in __all__.

--- unroll/__init__.py
+++ unroll/__init__.py
@@ -1,10 +1,12 @@
 """ unroll/__init__.py """
 
-__all__ = ['KeyStrikes',
-           'midi2strikes',
-           'video2rollscan',
-           'rollscan2keystrikes']
+__all__ = [
+    "KeyStrikes",
+    "midi2keystrikes",
+    "video2rollscan",
+    "rollscan2keystrikes",
+]
 
 from .KeyStrikes import KeyStrikes
 from .MIDI import midi2keystrikes
-from .video import video2rollscan, rollscan2keystrikes
+from .video import rollscan2keystrikes, video2rollscan
